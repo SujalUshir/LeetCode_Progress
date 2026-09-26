4a33fc287cc575a5b5a66ae66c9bf34d2771b0ff
Sujal Ushir <ushirthbsujal@gmail.com>
2026-09-26 13:08:43 +0530

    Time: 28 ms (5.21%), Space: 31.9 MB (5.01%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -6,21 +6,20 @@ public:
         int missing;
 
         set<int>s;
+        int sum=0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(s.find(grid[i][j])!=s.end()) repeated=grid[i][j];
-                else    s.insert(grid[i][j]);
+                sum+=grid[i][j];
+                if(s.find(grid[i][j])!=s.end()){
+                     repeated=grid[i][j];
+                }
+                s.insert(grid[i][j]);
             }
         }
 
-        for(int i=1;i<=n*n;i++){
-            if(s.find(i)==s.end()){
-                missing=i;
-                break;
-            }
-        }
+        
 
-        return {repeated,missing};
+        return {repeated,((n*n)*(n*n+1)/2)-sum+repeated};
     }
 };
